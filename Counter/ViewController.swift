7e55089433f337ba407731a38c4cfaa3af227816
LyashenkoKV <Lyashenko@inbox.ru>
2024-02-20 18:29:22 +0300

    Добавить лейблы счетчика

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 final class ViewController: UIViewController {
     
     @IBOutlet weak var resultsTextView: UITextView!
+    @IBOutlet weak var counterLabel: UILabel!
     private var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         resultsTextView.text = "История изменений:"
+        counterLabel.text = "0"
     }
     
     private func currentDate() -> String {
@@ -26,11 +28,13 @@ final class ViewController: UIViewController {
 
     @IBAction func plusActionButton(_ sender: Any) {
         counter += 1
+        counterLabel.text = String(counter)
         resultsTextView.text += "\n\(currentDate()) значение изменено на: \(counter)"
     }
     @IBAction func minusActionButton(_ sender: Any) {
         if counter > 0 {
             counter -= 1
+            counterLabel.text = String(counter)
             resultsTextView.text += "\n\(currentDate()) значение изменено на: \(counter)"
         } else {
             counter = 0
@@ -39,6 +43,7 @@ final class ViewController: UIViewController {
     }
     @IBAction func refreshActionButton(_ sender: Any) {
         counter = 0
+        counterLabel.text = String(counter)
         resultsTextView.text += "\n\(currentDate()) значение сброшено"
     }
 }
