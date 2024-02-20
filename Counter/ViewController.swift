9f01e67624ff43573a0b7a774adc9edcda32ae6f
LyashenkoKV <Lyashenko@inbox.ru>
2024-02-20 18:12:22 +0300

    Добавить логику вычисления

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultsTextView: UITextView!
+    private var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        resultsTextView.text = "История изменений:"
+    }
+    
+    private func currentDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let result = dateFormatter.string(from: Date())
+        return result
     }
 
-
+    @IBAction func plusActionButton(_ sender: Any) {
+        counter += 1
+        resultsTextView.text += "\n\(currentDate()) значение изменено на: \(counter)"
+    }
+    @IBAction func minusActionButton(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            resultsTextView.text += "\n\(currentDate()) значение изменено на: \(counter)"
+        } else {
+            counter = 0
+            resultsTextView.text += "\n\(currentDate()) попытка уменьшить значение счетчика ниже 0"
+        }
+    }
+    @IBAction func refreshActionButton(_ sender: Any) {
+        counter = 0
+        resultsTextView.text += "\n\(currentDate()) значение сброшено"
+    }
 }
 
